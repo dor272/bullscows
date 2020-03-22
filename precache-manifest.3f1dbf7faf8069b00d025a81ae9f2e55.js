a2595fd753c783633234cb7ebc3570884c5ed0fe
unknown <dor272@gmail.com>
2020-03-22 19:04:00 +0200

    Updates

---
+++ precache-manifest.3f1dbf7faf8069b00d025a81ae9f2e55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a96f0304fb230291a9bd726543375a5",
+    "url": "/bullscows/index.html"
+  },
+  {
+    "revision": "520a5221dbb415147269",
+    "url": "/bullscows/static/css/main.a70d07bd.chunk.css"
+  },
+  {
+    "revision": "e634ccbcd54421d4bcac",
+    "url": "/bullscows/static/js/2.7f59854f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bullscows/static/js/2.7f59854f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520a5221dbb415147269",
+    "url": "/bullscows/static/js/main.6e4d2d3a.chunk.js"
+  },
+  {
+    "revision": "2a939680ddad2cc3f3ae",
+    "url": "/bullscows/static/js/runtime-main.f250c5dc.js"
+  },
+  {
+    "revision": "c4c673ecbacb383817484666505c7437",
+    "url": "/bullscows/static/media/thumbsup.c4c673ec.png"
+  }
+]);
